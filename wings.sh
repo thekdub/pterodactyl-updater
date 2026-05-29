aaaa5795e13d9e8d7f23f5a39972c6048f4993d6
TheKDub <12961363+thekdub@users.noreply.github.com>
2026-05-29 17:14:38 -0400

    Update wings.sh
    Corrected version comparison. Adjusted installer to automatically determine amd64 or arm64 architecture. Added a message notifying  of current and new versions when updates are necessary.

--- wings.sh
+++ wings.sh
@@ -5,14 +5,18 @@ PATH_TO_INSTALLATION=/usr/local/bin/wings
 LATEST_VERSION=$(curl --silent https://cdn.pterodactyl.io/releases/latest.json | jq '.wings' | sed 's/"//g')
 CURRENT_VERSION=$(wings version | grep wings | awk '{ print substr ($0, 8 ) }')
 
-if [ "$LATEST_VERSION" = "$CURRENT_VERSION" ]
+if [ "v$LATEST_VERSION" = "$CURRENT_VERSION" ]
 then
   exit
 fi
 
-echo "Downloading latest Wings binary.."
-curl -L -o $PATH_TO_INSTALLATION https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_amd64
-echo "Applying permissions.."
+echo "Wings is outdated! Current version: $CURRENT_VERSION. Latest version: v$LATEST_VERSION"
+
+echo "Stopping Wings..."
+systemctl stop wings
+echo "Downloading latest Wings binary..."
+curl -L -o $PATH_TO_INSTALLATION "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
+echo "Applying permissions..."
 chmod u+x $PATH_TO_INSTALLATION
-echo "Restarting Wings.."
+echo "Restarting Wings..."
 systemctl restart wings
